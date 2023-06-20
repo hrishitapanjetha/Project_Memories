ded99cbff22d8ca9cbc891ef220db89744d12111
Hrishita Panjetha <hrishita.panjetha@gmail.com>
2023-06-20 21:50:26 +0530

    commit changes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 
 app.use('/posts', postRoutes);
@@ -13,10 +17,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://hrishitapanjetha:Rishita1122@cluster0.shzj2bh.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 3000;
+// const CONNECTION_URL = 'mongodb+srv://hrishitapanjetha:Rishita1122@cluster0.shzj2bh.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
